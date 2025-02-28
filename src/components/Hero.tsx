90e6413ea64ceaa303013e9a0b23af37dea30433
jesustissera <jesustissera@gmail.com>
2025-02-28 18:30:03 -0300

    We complete hero: add background image, text and opacity

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,20 @@
+import hero from '../assets/hero.jpeg'
+
 function Hero() {
     return (
-      <>
-        Hero
-      </>
+      <section className='relative flex items-center justify-center' style={{
+        backgroundImage: `url(${hero})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '90vh',
+
+      }}>
+        <h1 className='relative z-20 font-semibold max-w-6xl text-6xl text-white text-center'>
+          Servicio de venta y reparación de 
+          smartphones, tablets y computadoras
+        </h1>
+        <div className='absolute w-full h-full top-0 bg-black opacity-50 z-10'></div>
+      </section>
     );
   }
   
