3a34daf721cff7edb54fa044105bdb46003d045d
jesustissera <jesustissera@gmail.com>
2025-03-01 02:19:40 -0300

    Add four-column block in Choose Us section

--- src/components/ChooseUs.tsx
+++ src/components/ChooseUs.tsx
@@ -1,10 +1,153 @@
 function ChooseUs() {
-    return (
-      <>
-        ChooseUs
-      </>
-    );
-  }
-  
-  export default ChooseUs;
-  
+  return (
+    <section>
+      <div className="container mx-auto max-w-7xl py-10">
+        <h1 className="font-semibold text-6xl text-black text-center mx-auto">
+          ¿Por que elegirnos?
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 px-5 xl:px-0">
+          <div className="shadow-md rounded-3xl p-5 h-full flex flex-col">
+            <div>
+              <svg
+                width="43"
+                height="43"
+                viewBox="0 0 43 43"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3.99591 27.6599L38.6272 27.6599C39.3629 27.6599 39.9592 27.0635 39.9592 26.3279V7.68025C39.9592 6.94462 39.3629 6.34827 38.6272 6.34827L3.99591 6.34827C3.26029 6.34827 2.66394 6.94462 2.66394 7.68025V26.3279C2.66394 27.0635 3.26029 27.6599 3.99591 27.6599Z"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M5.3279 31.6558H37.2953M7.99184 35.6517H34.6313"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M21.3116 23.6639C24.9897 23.6639 27.9714 20.6822 27.9714 17.004C27.9714 13.3259 24.9897 10.3442 21.3116 10.3442C17.6334 10.3442 14.6517 13.3259 14.6517 17.004C14.6517 20.6822 17.6334 23.6639 21.3116 23.6639Z"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M39.9592 13.0081C38.1929 13.0081 36.4989 12.3065 35.25 11.0575C34.001 9.80854 33.2993 8.11457 33.2993 6.34827M2.66395 13.0081C3.53853 13.0081 4.40456 12.8359 5.21257 12.5012C6.02058 12.1665 6.75476 11.6759 7.37319 11.0575C7.99161 10.4391 8.48217 9.7049 8.81686 8.89689C9.15155 8.08888 9.32382 7.22285 9.32382 6.34827M39.9592 21C38.1929 21 36.4989 21.7016 35.25 22.9506C34.001 24.1996 33.2993 25.8935 33.2993 27.6598M2.66395 21C3.53853 21 4.40456 21.1722 5.21257 21.5069C6.02058 21.8416 6.75476 22.3322 7.37319 22.9506C7.99161 23.569 8.48217 24.3032 8.81686 25.1112C9.15155 25.9192 9.32382 26.7853 9.32382 27.6598"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+            <h3 className="text-xl font-bold">Presupuesto sin cargo</h3>
+            <p className="text-sm">
+              Traenos tu equipo, lo revisamos y te lo cotizamos.
+            </p>
+          </div>
+          <div className="shadow-md rounded-3xl p-5 h-full flex flex-col">
+            <div>
+              <svg
+                width="43"
+                height="43"
+                viewBox="0 0 43 43"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M21.3116 5.0163C12.4872 5.0163 5.32788 12.1757 5.32788 21C5.32788 29.8243 12.4872 36.9837 21.3116 36.9837C30.1359 36.9837 37.2952 29.8243 37.2952 21C37.2952 12.1757 30.1359 5.0163 21.3116 5.0163Z"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-miterlimit="10"
+                />
+                <path
+                  d="M21.3116 10.3442V22.3319H29.3034"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+            <h3 className="text-xl font-bold">Reparaciones Express</h3>
+            <p className="text-sm">
+              Trabajamos para que tu equipo esté listo lo antes posible porque
+              sabemos la necesidad de que no dejes de estar comunicado.
+            </p>
+          </div>
+          <div className="shadow-md rounded-3xl p-5 h-full flex flex-col">
+            <div>
+              <svg
+                width="43"
+                height="43"
+                viewBox="0 0 43 43"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15.3177 21C16.4212 21 17.3157 20.1055 17.3157 19.002C17.3157 17.8986 16.4212 17.0041 15.3177 17.0041C14.2143 17.0041 13.3198 17.8986 13.3198 19.002C13.3198 20.1055 14.2143 21 15.3177 21Z"
+                  fill="black"
+                />
+                <path
+                  d="M21.3157 31.6558C17.5346 31.6558 14.3545 29.1975 13.348 25.8426C13.3199 25.7427 13.3155 25.6377 13.3351 25.5358C13.3547 25.434 13.3979 25.3381 13.4611 25.2559C13.5243 25.1736 13.6059 25.1073 13.6993 25.0622C13.7927 25.0171 13.8953 24.9944 13.9991 24.9959H28.625C28.7287 24.9944 28.8313 25.0171 28.9247 25.0622C29.0181 25.1073 29.0997 25.1736 29.1629 25.2559C29.2261 25.3381 29.2693 25.434 29.2889 25.5358C29.3085 25.6377 29.3041 25.7427 29.276 25.8426C28.2778 29.1975 25.0969 31.6558 21.3157 31.6558Z"
+                  fill="black"
+                />
+                <path
+                  d="M27.3055 21C28.4089 21 29.3034 20.1055 29.3034 19.002C29.3034 17.8986 28.4089 17.0041 27.3055 17.0041C26.202 17.0041 25.3075 17.8986 25.3075 19.002C25.3075 20.1055 26.202 21 27.3055 21Z"
+                  fill="black"
+                />
+                <path
+                  d="M21.3116 38.3156C30.8747 38.3156 38.6272 30.5632 38.6272 21C38.6272 11.4368 30.8747 3.68433 21.3116 3.68433C11.7484 3.68433 3.99591 11.4368 3.99591 21C3.99591 30.5632 11.7484 38.3156 21.3116 38.3156Z"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-miterlimit="10"
+                />
+              </svg>
+            </div>
+            <h3 className="text-xl font-bold">Garantía y calidad</h3>
+            <p className="text-sm">
+              Usamos materiales y herramientas premium para garantizar la
+              reparación de cualquier equipo.
+            </p>
+          </div>
+          <div className="shadow-md rounded-3xl p-5 h-full flex flex-col">
+            <div>
+              <svg
+                width="43"
+                height="43"
+                viewBox="0 0 43 43"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M33.9654 7.68024H8.65788C6.08318 7.68024 3.99597 9.76744 3.99597 12.3421V29.6578C3.99597 32.2325 6.08318 34.3197 8.65788 34.3197H33.9654C36.5401 34.3197 38.6273 32.2325 38.6273 29.6578V12.3421C38.6273 9.76744 36.5401 7.68024 33.9654 7.68024Z"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M3.99597 15.6721H38.6273M10.6558 24.6629H14.6518V26.3279H10.6558V24.6629Z"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+            <h3 className="text-xl font-bold">Aceptamos tarjetas</h3>
+            <p className="text-sm">
+              Aceptamos tarjetas de crédito y débito. Consultanos por la cantidad
+              de cuotas y costos de financiación.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ChooseUs;
