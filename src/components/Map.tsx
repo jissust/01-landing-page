519c8c5961b7243906e5b8bdfb99f21e35ac3012
jesustissera <jesustissera@gmail.com>
2025-03-12 22:58:14 -0300

    I fixed the error with map, I indicated that the type could be map or null

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,20 +5,21 @@ import "@maptiler/sdk/dist/maptiler-sdk.css";
 
 function Map() {
   const mapContainer = useRef(null);
-  const map = useRef(null);
+  const map = useRef<maptilersdk.Map | null>(null);
   const tokyo = { lng: -58.6096, lat: -34.6539 };
   const zoom = 14;
   maptilersdk.config.apiKey = import.meta.env.VITE_API_KEY_MAPTILDER;
   
   useEffect(() => {
     if (map.current) return; // stops map from intializing more than once
-
-    map.current = new maptilersdk.Map({
-      container: mapContainer.current,
-      style: maptilersdk.MapStyle.STREETS,
-      center: [tokyo.lng, tokyo.lat],
-      zoom: zoom,
-    });
+    if(mapContainer.current !== null){    
+      map.current = new maptilersdk.Map({
+        container: mapContainer.current,
+        style: maptilersdk.MapStyle.STREETS,
+        center: [tokyo.lng, tokyo.lat],
+        zoom: zoom,
+      });
+    }
   }, [tokyo.lng, tokyo.lat, zoom]);
 
   return (
